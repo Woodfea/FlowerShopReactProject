682e812c7c5fbbef6a275c8d1ff759b1305faa6d
Guillaume COGONI <cogoni.guillaume@gmail.com>
2026-01-08 16:35:35 +0100

    fix: change PlantItem wrapper from div to li for semantic HTML
    Signed-off-by: Guillaume COGONI <cogoni.guillaume@gmail.com>

--- flower_shop_react_project/src/components/PlantItem.js
+++ flower_shop_react_project/src/components/PlantItem.js
@@ -3,14 +3,14 @@ import '../styles/PlantItem.css';
 
 function PlantItem({ name, cover, id, light, water }) {
     return (
-        <div key = { id } className="tfs-plant-item">
+        <li key = { id } className="tfs-plant-item">
             <img className='tfs-plant-item-cover' src={cover} alt={`${name} cover`}></img>
             {name}
             <div>
                 <CareScale careType='water' scaleValue={water} />
                 <CareScale careType='light' scaleValue={light} />
             </div>
-        </div>
+        </li>
     )
 }
 
