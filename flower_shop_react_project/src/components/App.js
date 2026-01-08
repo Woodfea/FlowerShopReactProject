6297dc7cffd1e14f012088413c1e45f26ceb5a43
Guillaume COGONI <cogoni.guillaume@gmail.com>
2026-01-08 16:34:38 +0100

    feat: add Footer component to App for improved layout
    Signed-off-by: Guillaume COGONI <cogoni.guillaume@gmail.com>

--- flower_shop_react_project/src/components/App.js
+++ flower_shop_react_project/src/components/App.js
@@ -2,12 +2,14 @@ import '../styles/App.css';
 import Banner from './Banner';
 /*import CartShop from './CartShop';*/
 import ShoppingList from './ShoppingList';
+import Footer from './Footer';
 
 function App() {
     return (
         <div>
             <Banner />
             <ShoppingList />
+            <Footer />
             {/*<CartShop />*/}
         </div>
     );
