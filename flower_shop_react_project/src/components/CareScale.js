59602b7381366b42162b388027ab91710b949496
Guillaume COGONI <cogoni.guillaume@gmail.com>
2026-01-08 16:35:21 +0100

    fix: correct care type symbols in CareScale component and adjust range

--- flower_shop_react_project/src/components/CareScale.js
+++ flower_shop_react_project/src/components/CareScale.js
@@ -2,21 +2,21 @@ import Water from '../assets/water.svg';
 import Sun from '../assets/sun.svg';
 
 function CareScale({ careType, scaleValue }) {
-    const range = [1, 2, 3, 4, 5];
+    const range = [1, 2, 3];
     let careTypeSymbol = '';
     switch (careType) {
         case 'water':
-            careTypeSymbol = Sun;
+            careTypeSymbol = Water;
             break;
         case 'light':
-            careTypeSymbol = Water;
+            careTypeSymbol = Sun;
             break;
         default:
             careTypeSymbol = '?';
             break;
     }
     return (
-        <div>
+        <div onClick={() => handleClick(careType, scaleValue)}>
             {
                 range.map((value) => {
                     if(scaleValue >= value) {
@@ -28,6 +28,27 @@ function CareScale({ careType, scaleValue }) {
         </div>
     );
 
+}
+
+
+function handleClick(careType, scaleValue) {
+    let careDescription = 'This plant requires ';
+
+    if (scaleValue === 1) {
+        careDescription += 'a little ';
+    } else if (scaleValue === 2) {
+        careDescription += 'some ';
+    } else if (scaleValue >= 3) {
+        careDescription += 'a lot of ';
+    }
+
+    if (careType === 'water') {
+        careDescription += "water";
+    } else if (careType === 'light') {
+        careDescription += "light";
+    }
+
+    alert(careDescription);
 }
 
 export default CareScale;
