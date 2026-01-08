abc9ef2665378d4f606891bd48b120dd7125de9d
Woodfea <>
2026-01-08 14:28:09 +0100

    feat: update CareScale component to use images for care type symbols

--- flower_shop_react_project/src/components/CareScale.js
+++ flower_shop_react_project/src/components/CareScale.js
@@ -1,12 +1,26 @@
+import Water from '../assets/water.svg';
+import Sun from '../assets/sun.svg';
+
 function CareScale({ careType, scaleValue }) {
     const range = [1, 2, 3, 4, 5];
-    const careTypeSymbol = careType === 'light' ? '☀️' : '💧';
+    let careTypeSymbol = '';
+    switch (careType) {
+        case 'water':
+            careTypeSymbol = Sun;
+            break;
+        case 'light':
+            careTypeSymbol = Water;
+            break;
+        default:
+            careTypeSymbol = '?';
+            break;
+    }
     return (
         <div>
             {
                 range.map((value) => {
                     if(scaleValue >= value) {
-                        return <span key={value.toString()}>{careTypeSymbol}</span>;
+                        return <img key={value.toString()} src={careTypeSymbol} alt={`${careType} care scale`} />;
                     }
                     return null;
                 })
