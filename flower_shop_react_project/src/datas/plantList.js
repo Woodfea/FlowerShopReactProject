4e2c53a0479d3718303678376de9c1a07fc4b71e
Woodfea <>
2026-01-08 14:27:31 +0100

    chore: add monstera image and update plantList with cover and light/water properties

--- flower_shop_react_project/src/datas/plantList.js
+++ flower_shop_react_project/src/datas/plantList.js
@@ -1,84 +1,78 @@
+import monstera from '../assets/monstera.jpg'
+
 export const plantList = [
 	{
 		name: 'monstera',
 		category: 'classique',
 		id: '1ed',
-        isBestSale: true,
-        isSpecialOffer: true,
-		water: 1,
-		light: 5
+		isBestSale: true,
+		light: 2,
+		water: 3,
+		cover: monstera
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
 		id: '2ab',
-        isBestSale: false,
-        isSpecialOffer: false,
-		water: 3,
-		light: 2
-		
+		light: 3,
+		water: 1,
+		cover: monstera
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
-        isBestSale: false,
-        isSpecialOffer: true,
-		water: 3,
-		light: 2
+		light: 1,
+		water: 2,
+		cover: monstera
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
 		id: '4kk',
-        isBestSale: false,
-        isSpecialOffer: false,
-		water: 2,
-		light: 2
+		light: 3,
+		water: 1,
+		cover: monstera
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
 		id: '5pl',
-        isBestSale: false,
-        isSpecialOffer: false,
+		light: 3,
 		water: 1,
-		light: 2
+		cover: monstera
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
 		id: '6uo',
-        isBestSale: false,
-        isSpecialOffer: false,
-		water: 3,
-		light: 3
+		light: 2,
+		water: 2,
+		cover: monstera
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
-        isBestSale: true,
-        isSpecialOffer: true,
+		isBestSale: true,
+		light: 2,
 		water: 3,
-		light: 4
+		cover: monstera
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
 		id: '8fp',
-        isBestSale: true,
-        isSpecialOffer: false,
-		water: 4,
-		light: 2
+		light: 2,
+		water: 1,
+		cover: monstera
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
 		id: '9vn',
-        isBestSale: false,
-        isSpecialOffer: false,
-		water: 5,
-		light: 1
+		light: 2,
+		water: 1,
+		cover: monstera
 	}
 ]
